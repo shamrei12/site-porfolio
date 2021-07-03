b882223787f91dfcd60ce9659b647ccc3cd72ab8
shamrei12 <61946511+shamrei12@users.noreply.github.com>
2021-07-03 17:34:51 +0300

    Big update. This update affected html, css and js files, as well as added pictures for information.
    What's included in the update:
    - Added a form for sending letters
    - Changed styles for the site
    - Modified slider with works
    - Added icons for information
    - Fixed bugs with the site
    - Fixed abrupt change of indentation before "About me"
    etc.

--- js/toogle-night-theme.js
+++ js/toogle-night-theme.js
@@ -3,10 +3,14 @@
 function toogle(){
     let toogle = document.querySelector('.toggle-button');
     let style = document.getElementById('style');
+    let style_text = document.querySelector('.text');
     if(toogle.checked){
         style.href="style-night.css";
+        style_text.innerHTML = "Темная тема";
+        
     }
     else{
         style.href="style.css"; 
+        style_text.innerHTML = "Светлая тема";
     }
 }
